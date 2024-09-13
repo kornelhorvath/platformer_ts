22ca8ecb341999f3934d98990af990018f10df23
Kornél Horváth <kornel.horvath@evocrm.hu>
2024-09-13 17:27:00 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const canvas: any = document.querySelector("#canvas1");
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+canvas.width = window.innerWidth * 0.95;
+canvas.height = window.innerHeight * 0.95;
+let topPos = canvas.getBoundingClientRect().top + window.scrollY;
+let leftPos = canvas.getBoundingClientRect().left + window.scrollX;
+let mouseX = leftPos;
+let mouseY = topPos;
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+class Game {
+  constructor(width: number, height: number){
+
+  }
+
+  update(ctx: CanvasRenderingContext2D, deltaTime: any){
+    console.log(deltaTime);
+  }
+
+  draw(ctx: CanvasRenderingContext2D){
+
+  }
+}
+
+
+const game = new Game(canvas.width, canvas.height);
+let lastTime = 0;
+function animate(timeStamp: any) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.update(ctx, deltaTime);
+  game.draw(ctx);
+  requestAnimationFrame(animate);
+}
+animate(0);
+
+
+
+
